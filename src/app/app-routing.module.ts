6db6d33acfc853cc6e46e548806bac64801a1f2a
Carlos Ortega <carloso2103@gmail.com>
2020-11-22 17:11:01 +0000

    Fix #1 Components and routes created for Home and Crud

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,16 @@
 import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
 import { Routes, RouterModule } from '@angular/router';
+import { CrudComponent } from './components/crud/crud.component';
+import { HomeComponent } from './components/home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent, pathMatch: 'full' },
+  { path: 'crud', component: CrudComponent },
+  { path: '**', redirectTo: '', pathMatch: 'full' }];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [BrowserModule, RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
